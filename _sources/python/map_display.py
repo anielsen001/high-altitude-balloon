e1339b634bf09af44c4cc84c8b1783fe84ce522e
Aaron Nielsen <apn@apnielsen.com>
2025-11-23 16:55:19 -0500

    created ouptut map

--- _sources/python/map_display.py
+++ _sources/python/map_display.py
@@ -22,8 +22,53 @@ def create_map(
     
     # read wspr csv file
     wspr = WsprTvCsv(wsprcsvfile)
+    llhd = wspr.llh(degrees=True,fill_nan=False)
+    latd = llhd[0,:]
+    lond = llhd[1,:]
+    times = wspr.times()
 
     # find the center of the map based on average
     # track point locations
-    lat_center = np.mean(wspr.lat(degrees=True))
-    lon_center = np.mean(wspr.lon(degrees=True))
+    lat_center = np.nanmean(latd)
+    lon_center = np.nanmean(lond)
+
+    # create a folium map
+    # default folium EPSG is EPSG:3857 or web-mercator
+    # GPS is EPSG:4326 or WGS-84
+    # however, the plots show correctly using EPSG:3857
+    m = folium.Map(
+        location = [lat_center,lon_center],
+        #crs = 'EPSG4326',
+    )
+
+    # draw the spots on the map as markers
+    for ii in range(len(times)):
+        _t = times.iloc[ii]
+        _llh = llhd[0:3,ii]
+        if np.isnan(_llh[2]):
+            fillcolor = '#F54927'
+        else:
+            fillcolor = '#21B55D'
+        marker = folium.vector_layers.CircleMarker(
+            _llh[0:2],
+            tooltip = str(_t),
+            radius=5,
+            fill=True,
+            fillColor=fillcolor,
+            fillOpacity=1.0,
+            )
+        marker.add_to(m)
+
+    # draw the track as a reference
+    llhgood = wspr.llh(degrees=True,no_nan=True)
+    track = folium.vector_layers.PolyLine(
+        llhgood[0:2,:].T,
+        ).add_to(m)
+    
+    # return the folium map object
+    return m
+
+if __name__=="__main__":
+    m = create_map(
+        '_sources/launches/smore-002/tracking/spots/KF8EEZ_109.csv',
+        'blah')
