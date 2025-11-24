cfd8a99ffc14fa91daef896a56be5470ce0f4da3
Aaron Nielsen <apn@apnielsen.com>
2025-11-23 21:35:47 -0500

    added helper functions

--- _sources/python/wsprtv.py
+++ _sources/python/wsprtv.py
@@ -7,6 +7,15 @@ import pandas as pd
 from pathlib import Path
 from scipy import interpolate as spi
 
+"""
+these are the columns as named in the csv file
+Index(['UTC Time', 'Grid', 'GPS Fix Validity', 'Track Attachment', 'Lat (°)',
+       'Lon (°)', 'Altitude (km)', 'Vertical Speed (m/min)', 'Speed (km/h)',
+       'Computed Speed (km/h)', 'Voltage (V)', 'Temperature (°C)',
+       'Sun Elevation (°)', '# RX Reports', 'Average RX Frequency (Hz)',
+       'Max RX Distance (km)', 'Max SNR (dB)''],
+"""
+
 class WsprTvCsv:
     """
     Class to read CSV files from WSPR Telemetry viewer
@@ -108,3 +117,60 @@ class WsprTvCsv:
         _dt = self._df['datetime']
         return _dt
 
+    def voltage(
+            self,
+            ):
+        """
+        return the voltage
+        """
+        _volt = self._df['Voltage (V)']
+        return _volt
+
+    def temperature(
+            self,
+            ):
+        """
+        return the temperature
+        """
+        _temp = self._df['Temperature (°C)']
+        return _temp
+        
+    def sun_elevation_angle(
+            self,
+            degrees = False,
+            ):
+        """
+        return the sun elevation angle
+        """
+        _angd = self._df['Sun Elevation (°)']
+        if degrees:
+            return _angd
+        else:
+            return np.radians(_angd)
+
+    def num_rx_spots(
+            self,
+            ):
+        """
+        return the number of received spots
+        """
+        _num = self._df['# RX Reports']
+        return _num
+
+    def max_rx_distance(
+            self,
+            ):
+        """
+        return the maximium spot receiver distance in meters
+        """
+        _max_dist = self._df['Max RX Distance (km)']
+        return _max_dist*1000.0
+
+    def ground_speed(
+            self,
+            ):
+        """
+        return the ground speed in kph
+        """
+        _spd = self._df['Speed (km/h)']
+        return _spd
