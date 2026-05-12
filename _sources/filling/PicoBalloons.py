136e7445e926f1693869e0d26bcbb32ba5397491
Aaron Nielsen <apn@apnielsen.com>
2026-05-12 05:30:02 -0400

    more working changes

--- _sources/filling/PicoBalloons.py
+++ _sources/filling/PicoBalloons.py
@@ -43,3 +43,114 @@ def gas_density(
     density = df_gas[df_gas['name']==gas_name][tempstr].to_numpy()*u.kg/u.m**3
 
     return density
+
+## Define atmospheric profile
+#fpath = "/opt/project/_sources/filling"
+# in this table, the P/T column is just the ration of the Pressure/(Temperature converted to Kelvin)
+df_atmos = pd.read_csv("atmos.csv")
+
+
+class Balloon():
+    weight = None
+    volume = None
+    name = None
+
+    def __init__(
+            self,
+            weight,
+            volume,
+            name=None,
+    ):
+        self.weight = weight
+        self.volume = volume
+        self.name= name
+
+    def namestr(self):
+        if self.name is None:
+            return f'Weight: {self.weight}, Volume: {self.volume}'
+        return f'{self.name}: Weight: {self.weight}, Volume: {self.volume}'
+
+    def __str__(self):
+        return self.namestr()
+
+    def __repr__(self):
+        return self.namestr()
+
+    def __add__(self,other):
+        return Balloon(self.weight + other.weight, self.volume + other.volume)
+
+    def __mul__(self,other):
+        return Balloon(self.weight * other, self.volume * other)
+
+    def __rmul__(self,other):
+        return Balloon(self.weight * other, self.volume * other)
+
+class BalloonSystem():
+
+    # optional
+    name = None
+    
+    # these are required
+    balloon = None
+    payload_weight = None
+    free_lift = None
+    gas = None
+
+    # these could be computed
+    gas_density = None
+    air_density = None
+    neck_lift = None
+    launch_gas_volume = None
+    lift_gas_mass = None
+    total_mass = None
+    system_density = None
+    free_lift_ratio = None
+
+    # these are computed float parameters
+    float_temperature = None
+    float_altitude = None
+    float_pressure = None
+    internal_pressure = None
+    differential_pressure = None
+    super_pressure = None
+    super_pressure_onset_altitude = None  
+
+    def __init__(
+            self,
+            balloon,
+            payload_weight,
+            free_lift,
+            gas,
+            name = None,
+            ):
+
+        # copy input parameters
+        self.balloon = balloon
+        self.payload_weight = payload_weight
+        self.free_lift = free_lift
+        self.gas = gas
+        self.name = name
+        
+        neck_lift = free_lift + payload_weight
+        lift_gas_density = gas_density(gas)
+        air_density = gas_density("air")
+        launch_gas_volume = (neck_lift + balloon.weight)/(air_density - lift_gas_density)
+        lift_gas_mass = lift_gas_density * launch_gas_volume
+        total_mass = lift_gas_mass + payload_weight + balloon.weight
+        system_density = total_mass / balloon.volume
+        free_lift_ratio = free_lift / (balloon.weight + payload_weight + lift_gas_mass)
+
+        temperature_at_float = atmos_temperature(system_density)
+        pressure_at_float = atmos_pressure(system_density)
+        balloon_altitude = atmos_altitude(system_density)
+
+        internal_pressure =
+            (atmosphere_pressure_msl * launch_gas_volume) / atmosphere_temperature_msl * ( temperature_at_float) / balloon.volume
+
+        differential_pressure = internal_pressure - pressure_at_float
+
+        kov = launch_gas_volume/balloon.volume * df_atmos.var"P/T (kPa/K)"[end]u"kPa/K"
+        super_pressure_onset_altitude =
+            LinearInterpolation(df_atmos.var"P/T (kPa/K)",
+                                df_atmos.var"Alt (m)")( (kov |> us"kPa/K").value  )u"m" # meter
+        super_pressure = pressure_at_float * free_lift_ratio
